af3a66ba2b9f0a9b8942c386ad967a9a363eaffc
FelixChen1 <felix.chen.zhuhai@software.dell.com>
2016-02-24 11:31:40 +0800

    add test project

---
+++ src/ForFun/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,14 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
+
